edcbb7c83a541488bc7143657af552765b4d97fe
hesicong <derekhe@april1985.com>
2022-06-28 20:40:06 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
+enum RadioMessage {
+    message1 = 49434
+}
 input.onButtonPressed(Button.A, function () {
     music.playMelody("- - - - - - - - ", 120)
+    if (true) {
+        servos.P0.setAngle(90)
+    } else {
+        radio.sendMessage(RadioMessage.message1)
+    }
+    radio.sendMessage(RadioMessage.message1)
+    music.ringTone(262)
 })
 basic.showNumber(0)
 basic.forever(function () {
